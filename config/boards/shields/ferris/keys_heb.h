200b35e59a01881df2424c606187d497fd87e2c2
jack_matanky <jackmatanky@gmail.com>
2023-01-26 08:32:09 +0200

    model hebrew keymap file on swedish keymap file from gusthavol

--- config/boards/shields/ferris/keys_heb.h
+++ config/boards/shields/ferris/keys_heb.h
@@ -6,9 +6,9 @@
 
 #pragma once
 
-#include <dt-bindings/zmk/hid_usage.h>
-#include <dt-bindings/zmk/hid_usage_pages.h>
-#include <dt-bindings/zmk/modifiers.h>
+// #include <dt-bindings/zmk/hid_usage.h>
+// #include <dt-bindings/zmk/hid_usage_pages.h>
+// #include <dt-bindings/zmk/modifiers.h>
 #include <dt-bindings/zmk/keys.h>
 
 // clang-format off

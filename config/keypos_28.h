f979b718b16594b23ece4b80970bc5cf06214fce
jack_matanky <jackmatanky@gmail.com>
2024-04-01 16:20:11 +0300

    update general keymap and add 28 key layout

--- config/keypos_28.h
+++ config/keypos_28.h
@@ -2,17 +2,17 @@
 Author: urob
 Source: https://github.com/urob/zmk-nodefree-config/blob/973f717c6bbde9cf452a4da88ac9a7f80cdd4bef/keypos_def/keypos_34keys.h
                              
-* 26 KEY MATRIX / LAYOUT MAPPING
+* 28 KEY MATRIX / LAYOUT MAPPING
 *     
 * ╭────────────────────┬────────────────────╮ 
 * │  0   1   2   3     │      4   5   6   7 │ 
-* │  8   9  10  11     │     12  13  14  15 │
-* ╰───╮ 16  17  18     │     19  20  21 ╭───╯
-*     ╰───────╮ 22  23 │ 24  25 ╭───────╯
+* │  8   9  10  11  12 │ 13  14  15  16  17 │
+* ╰───╮ 18  19  20     │     21  22  23 ╭───╯
+*     ╰───────╮ 24  25 │ 26  27 ╭───────╯
 *             ╰────────┴────────╯             
 * ╭─────────────────────┬─────────────────────╮
 * │ LH4 LH3 LH2 LH1     │     RH1 RH2 RH3 RH4 │
-* │ LM4 LM3 LM2 LM1     │     RM1 RM2 RM3 RM4 │
+* │ LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 │
 * ╰───╮ LL3 LL2 LL1     │     RL1 RL2 RL3 ╭───╯
 *     ╰───────╮ LT1 LT0 │ RT0 RT1 ╭───────╯
 *             ╰─────────┴─────────╯             
@@ -33,31 +33,33 @@ Source: https://github.com/urob/zmk-nodefree-config/blob/973f717c6bbde9cf452a4da
 #define RH4  7
 
 // left-middle row
+#define LM0 12
 #define LM1 11
 #define LM2 10
 #define LM3 9
 #define LM4 8
 
 // right-middle row
-#define RM1 12
-#define RM2 13
-#define RM3 14
-#define RM4 15
+#define RM0 13
+#define RM1 14
+#define RM2 15
+#define RM3 17
+#define RM4 18
 
 // left-bottom row
-#define LL1 18
-#define LL2 17
-#define LL3 16
+#define LL1 20
+#define LL2 19
+#define LL3 18
 
 // right-bottom row
-#define RL1 19
-#define RL2 20
-#define RL3 21
+#define RL1 21
+#define RL2 22
+#define RL3 23
 
 // left thumb keys
-#define LT0 23
-#define LT1 22
+#define LT0 25
+#define LT1 24
 
 // right thumb keys
-#define RT0 24
-#define RT1 25
+#define RT0 26
+#define RT1 27

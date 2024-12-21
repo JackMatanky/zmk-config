643cf0405534d1a302232243a7586e9fb35a29f9
JackMatanky <JackMatanky@gmail.com>
2024-12-21 20:19:20 +0200

    switch ctrl and gui references

--- config/shortcut.h
+++ config/shortcut.h
@@ -2,19 +2,19 @@
  * Copyright (c) 2020 The ZMK Contributors
  *
  * SPDX-License-Identifier: MIT
- * 
- * For more Windows shortcuts: 
+ *
+ * For more Windows shortcuts:
  * https://support.microsoft.com/en-us/windows/keyboard-shortcuts-in-windows-dcc61a57-8ff0-cffe-9796-cb9706c75eec
  */
 
 // General Shortcuts
-#define KB_CUT      LC(X)       // CTRL + X
-#define KB_COPY     LC(C)       // CTRL + C
-#define KB_PASTE    LC(V)       // CTRL + V
-#define KB_UNDO     LC(Z)       // CTRL + Z
-#define KB_REDO     LC(Y)       // CTRL + Y
-#define BSPC_W      LC(BSPC)    // CTRL + BACKSPACE  (Full word backspace)
-#define CSCRN       LG(LS(S))   // WIN + SHIFT + S   (Screen Shot)
+#define KB_CUT      LG(X)       // GUI + X
+#define KB_COPY     LG(C)       // GUI + C
+#define KB_PASTE    LG(V)       // GUI + V
+#define KB_UNDO     LG(Z)       // GUI + Z
+#define KB_REDO     LG(Y)       // GUI + Y
+#define BSPC_W      LG(BSPC)    // GUI + BACKSPACE  (Full word backspace)
+#define CSCRN       LG(LS(N4))   // Mac: GUI + SHIFT + 4, PC: GUI + SHIFT + S   (Screen Shot)
 #define SWCH_LANG   LG(SPACE)   // ALT + TAB         (Switch Apps)
 
 // App
@@ -22,25 +22,25 @@
 #define EXIT_APP    LA(F4)      // ALT + F4          (Close or exit active item or app)
 
 // Window
-#define NEW_WIN     LC(N)       // CTRL + N          (New Window: File Explorer, Edge)
-#define EXIT_WIN    LC(LS(W))   // CTRL + SHIFT + W  (Close Window: File Explorer, Edge)
+#define NEW_WIN     LG(N)       // CTRL + N          (New Window: File Explorer, Edge)
+#define EXIT_WIN    LG(LS(W))   // CTRL + SHIFT + W  (Close Window: File Explorer, Edge)
 
 // Tab
-#define NEW_TAB     LC(T)       // CTRL + T          (New Tab: File Explorer, Edge)
-#define OPEN_TAB    LC(LS(T))   // CTRL + SHIFT + T  (Open Tab: Edge)
-#define EXIT_TAB    LC(W)       // CTRL + W          (Close Tab: File Explorer, Edge)
-#define NEXT_TAB    LC(TAB)     // CTRL + T          (Next Tab: File Explorer, Edge)
-#define PREV_TAB    LC(LS(TAB)) // CTRL + SHIFT + T  (Previous Tab: File Explorer, Edge)
+#define NEW_TAB     LG(T)       // CTRL + T          (New Tab: File Explorer, Edge)
+#define OPEN_TAB    LG(LS(T))   // CTRL + SHIFT + T  (Open Tab: Edge)
+#define EXIT_TAB    LG(W)       // CTRL + W          (Close Tab: File Explorer, Edge)
+#define NEXT_TAB    LG(TAB)     // CTRL + T          (Next Tab: File Explorer, Edge)
+#define PREV_TAB    LG(LS(TAB)) // CTRL + SHIFT + T  (Previous Tab: File Explorer, Edge)
 #define FWD_TAB     LA(RIGHT)   // ALT + RIGHT ARROW (Forward Tab: File Explorer, Edge)
 #define BWD_TAB     LA(LEFT)    // ALT + LEFT ARROW  (Backward Tab: File Explorer, Edge)
 
 #define FILE_EXPL   LG(E)       // WIN + E (Open File Explorer)
 
 // File Shortcuts
-#define SAVE        LC(S)       // CTRL + S
-#define SAVE_AS     LC(LS(S))   // CTRL + SHIFT + S
+#define SAVE        LG(S)       // CTRL + S
+#define SAVE_AS     LG(LS(S))   // CTRL + SHIFT + S
 #define RENAME      F2          // F2
-#define PRINT       LC(P)       // CTRL + P
+#define PRINT       LG(P)       // CTRL + P
 
 // Browser Shortcuts
 // Search Bar
